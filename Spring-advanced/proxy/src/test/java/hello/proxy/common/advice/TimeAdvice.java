13a78d17807507e2f3f2ddca13879b7eaea33bc0
jojaeng2 <ds4ouj@naver.com>
2022-09-14 08:47:07 +0900

    (2022-09-14) Spring ProxyFactory 예제

--- Spring-advanced/proxy/src/test/java/hello/proxy/common/advice/TimeAdvice.java
+++ Spring-advanced/proxy/src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -9,6 +9,15 @@ public class TimeAdvice implements MethodInterceptor {
 
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        Object result = invocation.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
 
     }
 }
