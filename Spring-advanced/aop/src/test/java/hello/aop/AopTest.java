5b61d765ed8c00fd467c2073eafc2c770009f143
jojaeng2 <ds4ouj@naver.com>
2022-09-26 10:40:38 +0900

    (2022-09-26) Spring-AOP Pointcut 참조 학습

---
+++ Spring-advanced/aop/src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,46 @@
+package hello.aop;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest
+//@Import(AspectV2.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
+public class AopTest {
+
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService = {}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository = {}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        Assertions.assertThrows(IllegalStateException.class, () -> orderService.orderItem("ex"));
+    }
+}
