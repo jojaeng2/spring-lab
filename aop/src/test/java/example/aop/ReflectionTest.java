a72e576504392e15d74f9084fb250ec2ee2e208b
jojaeng2 <ds4ouj@naver.com>
2022-09-27 17:38:04 +0900

    (2022-09-27) Spring AOP Advice, Pointcut 학습

--- aop/src/test/java/example/aop/ReflectionTest.java
+++ aop/src/test/java/example/aop/ReflectionTest.java
@@ -2,9 +2,13 @@ package example.aop;
 
 import example.background.Hello;
 import example.background.HelloTarget;
+import example.background.UppercaseAdvice;
 import example.background.UppercaseHandler;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.lang.reflect.InvocationTargetException;
@@ -41,5 +45,31 @@ public class ReflectionTest {
                 new Class[] {Hello.class},
                 new UppercaseHandler(new HelloTarget())
         );
+    }
+
+    @Test
+    void proxyFactoryBean() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());     // 타깃 설정
+        pfBean.addAdvice(new UppercaseAdvice()); // 부가기능을 담은 어드바이스를 추가한다. 어드바이스는 여러 개가 추가될 수도 있다.
+
+        Hello proxyHello = (Hello) pfBean.getObject(); // getObject()로 생성된 프록시를 가져온다.
+    }
+
+    @Test
+    void pointcutAdvisor() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("sayH*");
+
+        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+
+        Hello proxyHello = (Hello) pfBean.getObject();
+
+        System.out.println(proxyHello.sayHello("Toby"));
+        System.out.println(proxyHello.sayHi("Toby"));
+        System.out.println(proxyHello.sayThankYou("Toby"));
     }
 }
