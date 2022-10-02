157ab83e0dcd4c47c1fa9f7b648bb045d9b8f112
jojaeng2 <ds4ouj@naver.com>
2022-10-02 11:39:10 +0900

    (2022-10-02) spring aop main 삭제

--- aop/src/test/java/example/aop/ReflectionTest.java
+++ aop/src/test/java/example/aop/ReflectionTest.java
@@ -17,59 +17,59 @@ import java.lang.reflect.Proxy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+//@SpringBootTest
 public class ReflectionTest {
 
 
-    @Test
-    void methodInterfaceTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        String name = "Spring";
-        Method lengthMethod = String.class.getMethod("length");
-
-        int length = (int) lengthMethod.invoke(name);
-        System.out.println("length = " + length);
-    }
-
-    @Test
-    void simpleProxy() {
-        Hello hello = new HelloTarget();
-        assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
-        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
-        assertThat(hello.sayThankYou("Toby")).isEqualTo("ThankYou Toby");
-    }
-
-    @Test
-    void dynamicProxy() {
-        Hello proxyHello = (Hello) Proxy.newProxyInstance(
-                getClass().getClassLoader(),
-                new Class[] {Hello.class},
-                new UppercaseHandler(new HelloTarget())
-        );
-    }
-
-    @Test
-    void proxyFactoryBean() {
-        ProxyFactoryBean pfBean = new ProxyFactoryBean();
-        pfBean.setTarget(new HelloTarget());     // 타깃 설정
-        pfBean.addAdvice(new UppercaseAdvice()); // 부가기능을 담은 어드바이스를 추가한다. 어드바이스는 여러 개가 추가될 수도 있다.
-
-        Hello proxyHello = (Hello) pfBean.getObject(); // getObject()로 생성된 프록시를 가져온다.
-    }
-
-    @Test
-    void pointcutAdvisor() {
-        ProxyFactoryBean pfBean = new ProxyFactoryBean();
-        pfBean.setTarget(new HelloTarget());
-
-        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
-        pointcut.setMappedNames("sayH*");
-
-        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
-
-        Hello proxyHello = (Hello) pfBean.getObject();
-
-        System.out.println(proxyHello.sayHello("Toby"));
-        System.out.println(proxyHello.sayHi("Toby"));
-        System.out.println(proxyHello.sayThankYou("Toby"));
-    }
+//    @Test
+//    void methodInterfaceTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+//        String name = "Spring";
+//        Method lengthMethod = String.class.getMethod("length");
+//
+//        int length = (int) lengthMethod.invoke(name);
+//        System.out.println("length = " + length);
+//    }
+//
+//    @Test
+//    void simpleProxy() {
+//        Hello hello = new HelloTarget();
+//        assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
+//        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
+//        assertThat(hello.sayThankYou("Toby")).isEqualTo("ThankYou Toby");
+//    }
+//
+//    @Test
+//    void dynamicProxy() {
+//        Hello proxyHello = (Hello) Proxy.newProxyInstance(
+//                getClass().getClassLoader(),
+//                new Class[] {Hello.class},
+//                new UppercaseHandler(new HelloTarget())
+//        );
+//    }
+//
+//    @Test
+//    void proxyFactoryBean() {
+//        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+//        pfBean.setTarget(new HelloTarget());     // 타깃 설정
+//        pfBean.addAdvice(new UppercaseAdvice()); // 부가기능을 담은 어드바이스를 추가한다. 어드바이스는 여러 개가 추가될 수도 있다.
+//
+//        Hello proxyHello = (Hello) pfBean.getObject(); // getObject()로 생성된 프록시를 가져온다.
+//    }
+//
+//    @Test
+//    void pointcutAdvisor() {
+//        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+//        pfBean.setTarget(new HelloTarget());
+//
+//        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+//        pointcut.setMappedNames("sayH*");
+//
+//        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+//
+//        Hello proxyHello = (Hello) pfBean.getObject();
+//
+//        System.out.println(proxyHello.sayHello("Toby"));
+//        System.out.println(proxyHello.sayHi("Toby"));
+//        System.out.println(proxyHello.sayThankYou("Toby"));
+//    }
 }
--- aop/src/test/java/example/aop/controller/BasicControllerTest.java
+++ aop/src/test/java/example/aop/controller/BasicControllerTest.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 public class BasicControllerTest {
 
-    @Autowired
-    private BasicController controller;
-
-    @Test
-    void 실행시간측정AOP적용() {
-        controller.receiveGetMapping(123);
-    }
+//    @Autowired
+//    private BasicController controller;
+//
+//    @Test
+//    void 실행시간측정AOP적용() {
+//        controller.receiveGetMapping(123);
+//    }
 }
